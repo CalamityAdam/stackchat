c6fc1432e44967cc77bfbdf499fbcadfa3af2253
Adam Sisk <adamsisk91@gmail.com>
2019-03-23 10:11:00 -0500

    initial commit

---
+++ client/index.js
@@ -0,0 +1,17 @@
+// Whoa?!? What is this?
+// Thanks to the style-loader and css-loader, webpack allows us import css,
+// and then auto-magically injects a <style> tag onto the DOM!
+// Wowzers! Check out the webpack.config.js to see how to add them!
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Main } from './components';
+
+ReactDOM.render(
+  <Router>
+    <Main />
+  </Router>,
+  document.getElementById('app')
+);
